5aa879cb9698994a33a998ae1804c3cc5bdc8ca4
nigel-riss <mr.kurenkov@gmail.com>
2018-05-23 17:27:30 +0200

    Start npm lecture

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,18 @@
-console.log('Starting app.');
+console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
+const notes = require('./notes');
 
 // console.log(os.userInfo());
 
-fs.appendFile('greetings.txt', `Hello: ${os.userInfo().username}`, function(err) {
-    if (err) {
-        console.log('Unable to write to file');
-    }
-});
+// fs.appendFile('greetings.txt', `Hello: ${os.userInfo().username}! You are ${notes.age}. \n`, function(err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
+
+// let res = notes.addNote();
+// console.log(res);
+console.log(notes.add(9, -2));

01b3033b6c5eb72b3d96b964078f8f0bac130aa2
nigel-riss <mr.kurenkov@gmail.com>
2018-05-29 15:59:31 +0200

    Add weather-app playground

---
+++ weather-app/async-basics.js
@@ -0,0 +1,12 @@
+console.log('Starting app');
+
+
+setTimeout(() => {
+    console.log('Inside of callback');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Zero timeout');
+}, 0);
+
+console.log('Finishing app');

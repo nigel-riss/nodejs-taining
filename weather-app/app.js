d8de9acba100723c833af655a2c0dc82db17f9f8
nigel-riss <mr.kurenkov@gmail.com>
2018-06-01 14:47:55 +0200

    Add yargs. Add address command and error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,34 @@
 const request = require('request');
+const yargs = require('yargs');
 
-request('https://maps.googleapis.com/maps/api/geocode/json?address=236005', {json: true}, (error, response, body) => {
-    console.log(body);
-});
+const googleAPIKey = 'AIzaSyAyR1WfQRqvsr5MEqd0sodQH-g-PwkXXtM'
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weathre for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+if (argv.address) {
+    request(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(argv.address)}&key=${googleAPIKey}`,
+        {json: true},
+        (error, response, body) => {
+            if (error) {
+                console.log('Unable to connect to Google servers.');
+            } else if (body.status === 'ZERO_RESULTS') {
+                console.log('Unable to find that address.');
+            } else if (body.status === 'OK') {
+                // console.log(JSON.stringify(body, undefined, 2));
+                console.log(`Address: ${body.results[0].formatted_address}`);
+                console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+                console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+            }
+    });
+}

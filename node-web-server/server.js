d85e6339cc6bef8f950efd9bac02eb0892ceb4fe
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2018-06-06 00:15:08 +0200

    Add express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
 const pug = require('pug');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 // app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.protocol}://${req.hostname}${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
